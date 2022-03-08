4e5a51840050e49254bfb5cf3ea2c221efe19f78
Jang Byung Yeon <kisagge@naver.com>
2022-03-08 15:50:20 +0900

    feat: BOJ 1402 아무래도이문제는A번난이도인것같다

---
+++ BOJ/1001~2000/1402.py
@@ -0,0 +1,6 @@
+# BaekJoon Onlin Judge - 1402번 아무래도이문제는A번난이도인것같다
+import sys
+T = int(sys.stdin.readline().rstrip())
+for i in range(T):
+    A, B = map(int, sys.stdin.readline().rstrip().split())
+    print('yes')
